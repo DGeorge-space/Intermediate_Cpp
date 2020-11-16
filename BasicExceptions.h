4537e37a633d338d4b4693d0deb972508e337495
Dan George <danieljamesgeorge@gmail.com>
2020-11-16 19:40:04 +0000

    basic Exception2

--- BasicExceptions.h
+++ BasicExceptions.h
@@ -4,28 +4,40 @@
 
 using namespace std;
 
-// can throw in any function and then the exception will occur wherever the catch is up the call stack: 
+// can throw in any function and then the exception will occur wherever the catch is up the call stack:
 
-
+// in practice this means we can try and throw inside of a function, but not catch as late as main:
 void mightGoWrong()
 {
 
-    bool error1 = true;
+    bool error1 = false;
     bool error2 = true;
 
-    try
+    if (error1)
     {
-
-        if (error1)
-        {
-            // throw 8; // will through the value 8 if an error occurs "terminate called after throwing an instance of 'int'"
-            throw "hmmm that's strange";        
-        }
-        if (error2){
-            throw string("Something else has gone wrong ") // use the string class name rather than array of int 
-                // do not need to use the new keyword here because the function is defined to instantiate
-        }
+        // throw 8; // will through the value 8 if an error occurs "terminate called after throwing an instance of 'int'"
+        throw "hmmm that's strange";
     }
+    if (error2)
+    {
+        throw string("Something else has gone wrong ") ;// use the string class name rather than array of int
+                                                       // do not need to use the new keyword here because the function is defined to instantiate
+    }
+}
+void usesMightGoWrong()
+{
+    mightGoWrong();
+    cout << "If you are reading this, our exception hasn't been caught yet!" <<endl;
+}
+
+
+
+void catchIfWrong()
+{
+    try { usesMightGoWrong();
+    
+    }
+        
     catch (int err)
     {
         cout << " Error code: " << err << endl;
--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 int main()
 {
-    mightGoWrong();
+    catchIfWrong();
     cout <<"" << endl;
 }
